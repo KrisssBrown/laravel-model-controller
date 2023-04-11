7d7e676839353981eeb06b63210b65a83d6b0602
KrisssBrown <cristianbruno.cs@gmail.com>
2023-04-11 21:07:12 +0200

    EDIT PageController

---
+++ app/Http/Controllers/Guest/PageController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Guest;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Movie;
+
+
+class PageController extends Controller
+{
+    public function index() {
+        $movies = Movie::all();
+
+        $data =[
+            'movies' => $movies
+        ];
+
+        return view('layouts.movies');
+    }//
+
+    // public function items(){
+    // }
+}
---
+++ resources/views/layouts/movies.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    <!-- Styles -->
+    @vite('resources/js/app.js')
+
+</head>
+
+<body>
+
+    <div class="container">
+        @foreach ($Movies as $Movie)
+            <div class="card text-white bg-primary mb-3" style="max-width: 18rem;">
+                <div class="card-header">Header</div>
+                <div class="card-body">
+                    <h5 class="card-title">Primary card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                        card's content.</p>
+                </div>
+            </div>
+        @endforeach
+    </div>
+
+</body>
+
+</html>
